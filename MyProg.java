53c89ebf6bb5cb7fb6e8808e01b17574d40d60ff
NinaZima <90524302+NinaZima@users.noreply.github.com>
2021-09-17 21:29:52 +0300

    Add files via upload

---
+++ MyProg.java
@@ -0,0 +1,13 @@
+class MyProg {
+	public static void main(String[] args) {
+     int a = 8;
+     int b = 11;
+     if (a > b ) {
+        System.out.println("Махатма" );
+        }
+    else {
+        System.out.println("Джавахарлал" );
+        }
+		
+	}
+}
